9759ff7d60c80f0d83193e72863bd53ef632fd7a
taukane <taukane@yahoo.com.br>
2026-06-02 06:58:41 -0300

    Vite Base

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  base: 'https://github.com/taukane/taukvue/',
+  base: '/taukvue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
